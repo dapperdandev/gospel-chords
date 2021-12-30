0fa464d16e3ad5335360f9b592b1bf403111f139
Daniel Breen <daninreallife7@gmail.com>
2021-12-30 10:51:12 -0600

    initial commit

---
+++ apps/web/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gospel-link-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'web';
+}
